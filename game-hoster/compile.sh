a5ad53cebd9479c961f2ad06275bab41de0b8fa5
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-02 15:03:19 -0800

    updates

--- game-hoster/compile.sh
+++ game-hoster/compile.sh
@@ -16,7 +16,15 @@ for ai in "${projects[@]}"; do
   for file in "${project_files[@]}"; do 
     if [[ "${file}" == "Cargo.toml" ]]; then  
       rust_cargo=true
-      break  
+      if [ rust_cargo ]; then 
+        echo "uncovered cargo project"
+        # proceed with compiling and marking exec as a contestent in a folder
+        folder_to_copy=($(pwd))
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
+      fi
+      break
     fi
   done
 
