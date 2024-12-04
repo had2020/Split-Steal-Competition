b82be79ebacee3b99525ccec106655ff422b5264
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-04 10:31:18 -0800

    updates

---
+++ base-ai/constant_split_python/constant_split.py
@@ -0,0 +1,26 @@
+import random
+
+def first_hand():
+    """Generates a random number (0 or 1) and prints it."""
+    num = random.randint(0, 1)
+    print(num)
+
+def understand_response():
+    """Reads user input and returns a boolean based on the input."""
+    enemy_move = input()
+    return enemy_move == "0"
+
+def turn(split):
+    """Prints the next move based on the split value."""
+    next_move = "1" if split else "0"
+    print(next_move)
+    game_loop()
+
+def game_loop():
+    """Starts a new game loop by reading the enemy's move and calling the 'turn' function."""
+    enemy_move = understand_response()
+    turn(enemy_move)
+
+if __name__ == "__main__":
+    first_hand()
+    game_loop()
--- game-hoster/compile.sh
+++ game-hoster/compile.sh
@@ -1,9 +1,15 @@
+# checking for sudo
+if [ "$(id -u)" -ne 0 ]; then
+    echo "You need to run this script as root."
+    exit 1
+fi
+
 cd ..
+mkdir contestants
 cd base-ai
 projects=($(ls))
 ai_dir=($(pwd))
-
-mkdir contestants
+echo "searhing for bots in ai_dir..."
 
 # Comment
 for ai in "${projects[@]}"; do 
@@ -14,32 +20,56 @@ for ai in "${projects[@]}"; do
   # process in here
   project_files=($(ls))
   rust_cargo=false
+  python_project=false
+  cplusplus=false
 
-  for file in "${project_files[@]}"; do 
+  for file in "${project_files[@]}"; do
+    #echo "$file"
     if [[ "${file}" == "Cargo.toml" ]]; then  
       rust_cargo=true
       if [ rust_cargo ]; then 
-        echo "uncovered cargo project"
+        #echo "uncovered cargo project"
         # proceed with compiling and marking exec as a contestent in a folder
         folder_to_copy=($(pwd))
         cd ..
         #echo "($(ls))"
         cd ..
         cd contestants
-        echo "($(ls))"
-        #cp -r "$folder_to_copy" .
+        cp -r "$folder_to_copy" .
       fi
       break
+    elif [[ "${file}"  == *.py ]]; then
+        python_project=true
+        folder_to_copy=($(pwd))
+        cd ..
+        #echo "($(ls))"
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
+    elif [[ "${file}"  == *.cpp ]]; then
+        cplusplus=true
+        folder_to_copy=($(pwd))
+        cd ..
+        #echo "($(ls))"
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
     fi
   done
 
-  if [ $rust_cargo == true ]; then  
-    ls
-  fi
+
+  #if [ $rust_cargo == true ]; then  #does work, for later use
+    #echo "TODO something to rust cargo projects ie export them"
+  #elif [ $python_project == true ]; then
+    #echo "TODO something with python projects"
+  #elif [ $cplusplus == true ]; then
+    #echo "TODO something with cpluscplus projects"
+  #fi
 
   cd "$ai_dir"  
 done
 
-
-echo "game complete"
+#echo "game complete"
+#echo "($(ls))"
+cd ..
 rm -rf contestants
