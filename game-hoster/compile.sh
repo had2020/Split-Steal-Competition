197eadff75fb24df6894d044800bf372bb0cebc3
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-05 10:45:06 -0800

    updates

--- game-hoster/compile.sh
+++ game-hoster/compile.sh
@@ -1,28 +1,7 @@
-# checking for sudo
-if [ "$(id -u)" -ne 0 ]; then
-    echo "You need to run this script as root."
-    exit 1
-fi
-
+echo "compiling bots..."
 cd ..
-mkdir contestants
-cd base-ai
-projects=($(ls))
-ai_dir=($(pwd))
-echo "searhing for bots in ai_dir..."
 
-# Comment
-for ai in "${projects[@]}"; do 
-  #echo "$ai" #prints name
-  #echo "$ai: $(file --brief "$ai")"
-  #ls
-  cd "$ai"
-  # process in here
-  project_files=($(ls))
-  rust_cargo=false
-  python_project=false
-  cplusplus=false
-  counter=1
+cd contestants
 
   for file in "${project_files[@]}"; do
     #echo "$file"
@@ -37,9 +16,7 @@ for ai in "${projects[@]}"; do
         cd ..
         cd contestants
         cp -r "$folder_to_copy" .
-        ((counter++))
-      fi
-      break
+        fi
     elif [[ "${file}"  == *.py ]]; then
         python_project=true
         folder_to_copy=($(pwd))
@@ -48,7 +25,6 @@ for ai in "${projects[@]}"; do
         cd ..
         cd contestants
         cp -r "$folder_to_copy" .
-        ((counter++))
     elif [[ "${file}"  == *.cpp ]]; then
         cplusplus=true
         folder_to_copy=($(pwd))
@@ -57,26 +33,8 @@ for ai in "${projects[@]}"; do
         cd ..
         cd contestants
         cp -r "$folder_to_copy" .
-        ((counter++))
     fi
   done
-  echo "count: $counter"
 
-  #if [ $rust_cargo == true ]; then  #does work, for later use
-    #echo "TODO something to rust cargo projects ie export them"
-  #elif [ $python_project == true ]; then
-    #echo "TODO something with python projects"
-  #elif [ $cplusplus == true ]; then
-    #echo "TODO something with cpluscplus projects"
-  #fi
-
-  cd "$ai_dir"  
-done
-
-#echo "game complete"
-#echo "($(ls))"
 cd ..
-
-#TODO something with files/folders in contestants
-
 rm -rf contestants
---
+++ game-hoster/gather.sh
@@ -0,0 +1,79 @@
+# checking for sudo
+if [ "$(id -u)" -ne 0 ]; then
+    echo "You need to run this script as root."
+    exit 1
+fi
+
+cd ..
+mkdir contestants
+cd base-ai
+projects=($(ls))
+ai_dir=($(pwd))
+echo "searhing for bots in ai_dir..."
+
+# Comment
+for ai in "${projects[@]}"; do 
+  #echo "$ai" #prints name
+  #echo "$ai: $(file --brief "$ai")"
+  #ls
+  cd "$ai"
+  # process in here
+  project_files=($(ls))
+  rust_cargo=false
+  python_project=false
+  cplusplus=false
+
+  for file in "${project_files[@]}"; do
+    #echo "$file"
+    if [[ "${file}" == "Cargo.toml" ]]; then  
+      rust_cargo=true
+      if [ rust_cargo ]; then 
+        #echo "uncovered cargo project"
+        # proceed with compiling and marking exec as a contestent in a folder
+        folder_to_copy=($(pwd))
+        cd ..
+        #echo "($(ls))"
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
+      fi
+      break
+    elif [[ "${file}"  == *.py ]]; then
+        python_project=true
+        folder_to_copy=($(pwd))
+        cd ..
+        #echo "($(ls))"
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
+    elif [[ "${file}"  == *.cpp ]]; then
+        cplusplus=true
+        folder_to_copy=($(pwd))
+        cd ..
+        #echo "($(ls))"
+        cd ..
+        cd contestants
+        cp -r "$folder_to_copy" .
+    fi
+  done
+
+  #if [ $rust_cargo == true ]; then  #does work, for later use
+    #echo "TODO something to rust cargo projects ie export them"
+  #elif [ $python_project == true ]; then
+    #echo "TODO something with python projects"
+  #elif [ $cplusplus == true ]; then
+    #echo "TODO something with cpluscplus projects"
+  #fi
+
+  cd "$ai_dir"  
+done
+
+#echo "game complete"
+#echo "($(ls))"
+cd ..
+
+#TODO something with files/folders in contestants
+echo "Finished compiling bots"
+cd game-hoster
+sh compile.sh
+#rm -rf contestants
