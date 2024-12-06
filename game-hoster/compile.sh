486abf8b9acb4215c9d4beede0c7dd683cc33bfa
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-06 13:41:09 -0800

    Update compile.sh

--- game-hoster/compile.sh
+++ game-hoster/compile.sh
@@ -1,22 +1,36 @@
 echo "compiling bots..."
 cd ..
 
-cd contestants
+mkdir builds
 
+cd contestants
 projects=($(ls))
 
 for ai in "${projects[@]}"; do 
   cd "$ai"
   project_files=($(ls))
-
   for file in "${project_files[@]}"; do
-    echo "$file"
+    #echo "$file"
     if [[ "${file}" == "Cargo.toml" ]]; then  
         echo "rust"
+        export CARGO_TARGET_DIR=../../builds
+        #cargo build
+        cargo build --release
+        cd ..
     elif [[ "${file}"  == *.py ]]; then
-        echo "python"
+        echo "python" # Nothing else since we will just run python with python
+        cd ..
     elif [[ "${file}"  == *.cpp ]]; then
         echo "cpp"
+        g++ -o ${file} ${file}.cpp 
+        cd ..
     fi
   done
 done
+
+cd ..
+rm -rf contestants
+rm -rf builds #for main.rs to handle
+
+cd game-hoster
+cargo run
