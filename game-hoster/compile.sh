4828fcde01601e7ef6ba0caa5fc4d7913859fbca
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-11 12:04:55 -0800

    dsf

--- game-hoster/compile.sh
+++ game-hoster/compile.sh
@@ -21,26 +21,32 @@ for ai in "${projects[@]}"; do
         #export CARGO_TARGET_DIR=../
         #cargo build --release --quiet
         cargo build --release --quiet
-        mv -v "${file}" ${builds_folder}
+        #mv -v "${file}" ${builds_folder}
+        parent_dir=($(dirname "$PWD"))
+        cd target/release
+        folder=($(pwd))
+        #cp -r "$(folder)/$(parent_dir)"
+        echo "yo"
+        echo "${folder}/${parent_dir}"
         #cargo build --artifact-dir=/ --target-dir=../../builds --release --no-default-features --features=bin
         cd ..
     elif [[ "${file}"  == *.py ]]; then
         echo " Python " >> build_log.txt
         #mv "${file}" lol.py
         #echo "($(ls))"
-        mv -v "${file}" ${builds_folder}
+        #mv -v "${file}" ${builds_folder}
         cd ..
     elif [[ "${file}"  == *.cpp ]]; then
         echo " C++ " >> build_log.txt 
         g++ -o ${file} ${file}.cpp 
-        mv "${file}" ../builds
+        #mv "${file}" ../builds
         cd ..
     fi
   done
 done
 
 cd ..
-rm -rf contestants
+#rm -rf contestants
 # TODO /\ old code, make only exec and python files go into builds
 
 #rm -rf builds #for main.rs to handle
